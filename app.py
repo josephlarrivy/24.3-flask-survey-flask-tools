87cf492d983b0a070ea275e1b32b90afff005d6e
josephlarrivy <joseph.larrivy@gmail.com>
2022-09-20 18:17:08 -0500

    assingment complete - need to learn about sessions

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, request, render_template
+from flask import Flask, request, render_template, redirect, flash, session
 
 app = Flask(__name__)
 
@@ -13,6 +13,9 @@ from surveys import satisfaction_survey as survey
 
 
 
+
+
+
 responses = []
 
 @app.route('/')
@@ -20,8 +23,50 @@ def start_page():
     return render_template('/start_page.html', survey=survey)
 
 
+@app.route("/begin", methods=["POST"])
+def start_survey():
+    return redirect('/questions/0')
 
 
+
+
+
+
+@app.route('/questions/<int:qnum>', methods= ['GET'])
+def questions(qnum):
+
+    question = survey.questions[qnum]
+    
+
+    # if the lenght of the responses list is the same as the length of the questions list, the survey must be complete
+    if (len(responses) == len(survey.questions)):
+        return redirect("/complete")
+
+    if (len(responses) != qnum and qnum>0):
+        flash("Invalid. Please answer questions in order.")
+        return redirect(f'/questions/{len(responses)}')
+
+    
+    return render_template('/questions.html', question_num=qnum, question=question)
+
+
+
+
+@app.route('/answer', methods=['POST'])
+def answer():
+    choice = request.form['answer']
+    responses.append(choice)
+
+    if (len(responses) == len(survey.questions)):
+        return redirect("/complete")
+
+    else:
+        return redirect(f"/questions/{len(responses)}")
+
+
+@app.route('/complete')
+def survey_complete():
+    return render_template('complete.html')
 
 
 
